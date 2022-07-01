eb411b44f4d96e3d2542829bd99b8f48d4d00cdc
Adam Blanchet <ab59@sanger.ac.uk>
2022-07-01 11:04:09 +0000

    conftest.py: Add TestClient dependency override of QC DB connection.
    Also add QC DB connection section to testdb.ini

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,14 @@ import os
 
 import pytest
 from fastapi.testclient import TestClient
-from ml_warehouse.schema import Base
+from ml_warehouse.schema import Base as MlwhBase
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, Session
 
 from lang_qc.main import app
 from lang_qc.db.mlwh_connection import get_mlwh_db
+from lang_qc.db.qc_connection import get_qc_db
+from lang_qc.db.qc_schema import Base as QcBase
 
 test_ini = os.path.join(os.path.dirname(__file__), "testdb.ini")
 
@@ -22,19 +24,19 @@ def config() -> configparser.ConfigParser:
     return test_config
 
 
-def mysql_url(config: configparser.ConfigParser):
+def mlwh_mysql_url(config: configparser.ConfigParser):
     """Returns a MySQL URL configured through an ini file.
 
     The keys and values are:
 
-    [MySQL]
-    user       = <database user, defaults to "mlwh">
+    [MySQL MLWH]
+    user       = <database user, defaults to "test">
     password   = <database password, defaults to empty i.e. "">
     ip_address = <database IP address, defaults to "127.0.0.1">
     port       = <database port, defaults to 3306>
-    schema     = <database schema, defaults to "mlwh">
+    schema     = <database schema, defaults to "mlwarehouse">
     """
-    section = "MySQL"
+    section = "MySQL MLWH"
 
     if section not in config.sections():
         raise configparser.Error(
@@ -43,11 +45,44 @@ def mysql_url(config: configparser.ConfigParser):
             "tests on a {} database".format(section, section)
         )
     connection_conf = config[section]
-    user = connection_conf.get("user", "mlwh")
+    user = connection_conf.get("user", "test")
     password = connection_conf.get("password", "")
     ip_address = connection_conf.get("ip_address", "127.0.0.1")
     port = connection_conf.get("port", "3306")
-    schema = connection_conf.get("schema", "mlwh")
+    schema = connection_conf.get("schema", "mlwarehouse")
+
+    return (
+        f"mysql+pymysql://{user}:{password}@"
+        f"{ip_address}:{port}/{schema}?charset=utf8mb4"
+    )
+
+
+def qcdb_mysql_url(config: configparser.ConfigParser):
+    """Returns a MySQL URL configured through an ini file.
+
+    The keys and values are:
+
+    [MySQL QCDB]
+    user       = <database user, defaults to "test">
+    password   = <database password, defaults to empty i.e. "">
+    ip_address = <database IP address, defaults to "127.0.0.1">
+    port       = <database port, defaults to 3307>
+    schema     = <database schema, defaults to "langqc">
+    """
+    section = "MySQL QCDB"
+
+    if section not in config.sections():
+        raise configparser.Error(
+            "The {} configuration section is missing. "
+            "You need to fill this in before running "
+            "tests on a {} database".format(section, section)
+        )
+    connection_conf = config[section]
+    user = connection_conf.get("user", "test")
+    password = connection_conf.get("password", "")
+    ip_address = connection_conf.get("ip_address", "127.0.0.1")
+    port = connection_conf.get("port", "3307")
+    schema = connection_conf.get("schema", "langqc")
 
     return (
         f"mysql+pymysql://{user}:{password}@"
@@ -57,7 +92,7 @@ def mysql_url(config: configparser.ConfigParser):
 
 @pytest.fixture(scope="function", name="mlwhdb_test_sessionfactory")
 def create_mlwhdb_test_sessionfactory(config):
-    engine = create_engine(mysql_url(config), future=True)
+    engine = create_engine(mlwh_mysql_url(config), future=True)
 
     TestingSessionLocal = sessionmaker(bind=engine)
 
@@ -69,14 +104,29 @@ def create_mlwhdb_test_sessionfactory(config):
         conn.execute(text("SET foreign_key_checks=0;"))
         conn.commit()
     # Drop all tables and then create them to make it easier to test locally too.
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
+    MlwhBase.metadata.drop_all(bind=engine)
+    MlwhBase.metadata.create_all(bind=engine)
+
+    return TestingSessionLocal
+
+
+@pytest.fixture(scope="function", name="qcdb_test_sessionfactory")
+def create_qcdb_test_sessionfactory(config):
+    engine = create_engine(qcdb_mysql_url(config), future=True)
+
+    TestingSessionLocal = sessionmaker(bind=engine)
+
+    # Drop all tables and then create then to make it easier to test locally too.
+    QcBase.metadata.drop_all(bind=engine)
+    QcBase.metadata.create_all(bind=engine)
 
     return TestingSessionLocal
 
 
 @pytest.fixture(scope="function", name="test_client")
-def create_test_client(config, mlwhdb_test_sessionfactory) -> TestClient:
+def create_test_client(
+    config, mlwhdb_test_sessionfactory, qcdb_test_sessionfactory
+) -> TestClient:
     def override_get_mlwh_db():
         try:
             db: Session = mlwhdb_test_sessionfactory()
@@ -84,8 +134,15 @@ def create_test_client(config, mlwhdb_test_sessionfactory) -> TestClient:
         finally:
             db.close()
 
-    app.dependency_overrides[get_mlwh_db] = override_get_mlwh_db
+    def override_get_qc_db():
+        try:
+            db: Session = qcdb_test_sessionfactory()
+            yield db
+        finally:
+            db.close()
 
+    app.dependency_overrides[get_mlwh_db] = override_get_mlwh_db
+    app.dependency_overrides[get_qc_db] = override_get_qc_db
     client = TestClient(app)
 
     return client
