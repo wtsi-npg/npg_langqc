82f6c0ae3a594007505e153236ac77b82a7e3861
Adam Blanchet <ab59@sanger.ac.uk>
2022-07-12 15:49:18 +0000

    qc_state.py: add some exceptions to be returned by endpoint.
    Signed-off-by: Adam Blanchet <ab59@sanger.ac.uk>

--- lang_qc/endpoints/qc_state.py
+++ lang_qc/endpoints/qc_state.py
@@ -17,16 +17,17 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
-from dataclasses import dataclass
 from datetime import datetime
 import json
 from typing import Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from ml_warehouse.schema import PacBioRunWellMetrics
 from product_id.main import PacBioWell
 from sqlalchemy import and_, select
 from sqlalchemy.orm import Session
 
+from lang_qc.db.mlwh_connection import get_mlwh_db
 from lang_qc.db.qc_connection import get_qc_db
 from lang_qc.db.qc_schema import (
     ProductLayout,
@@ -97,11 +98,6 @@ def create_well_properties_digest(run_name, well_label):
     return hash(frozenset(create_well_properties(run_name, well_label)))
 
 
-@dataclass
-class UserWrapper:
-    name: str
-
-
 def construct_seq_product_for_well(
     run_name: str, well_label: str, qcdb_session: Session
 ):
@@ -109,66 +105,108 @@ def construct_seq_product_for_well(
     seq_platform = qcdb_session.execute(
         select(SeqPlatform).where(SeqPlatform.name == "PacBio")
     ).scalar_one_or_none()
+    if seq_platform is None:
+        raise Exception("PacBio SeqPlatform is not in the QC database.")
+
     run_name_product_attr = qcdb_session.execute(
         select(SubProductAttr).where(SubProductAttr.attr_name == "run_name")
     ).scalar_one_or_none()
+    if run_name_product_attr is None:
+        raise Exception("PacBio run name SubProductAttr is not the QC database.")
+
     well_label_product_attr = qcdb_session.execute(
         select(SubProductAttr).where(SubProductAttr.attr_name == "well_label")
     ).scalar_one_or_none()
+    if well_label_product_attr is None:
+        raise Exception("PacBio well label SubProductAttr is not in the QC database.")
 
-    qcdb_session.add(
-        SeqProduct(
-            id_product=create_id_product(run_name, well_label),
-            seq_platform=seq_platform,
-            product_layout=[
-                ProductLayout(
-                    sub_product=SubProduct(
-                        sub_product_attr=run_name_product_attr,
-                        sub_product_attr_=well_label_product_attr,
-                        value_attr_one=run_name,
-                        value_attr_two=well_label,
-                        properties=create_well_properties(run_name, well_label),
-                        properties_digest=create_well_properties_digest(
-                            run_name, well_label
-                        ),
-                    )
+    seq_product = SeqProduct(
+        id_product=create_id_product(run_name, well_label),
+        seq_platform=seq_platform,
+        product_layout=[
+            ProductLayout(
+                sub_product=SubProduct(
+                    sub_product_attr=run_name_product_attr,
+                    sub_product_attr_=well_label_product_attr,
+                    value_attr_one=run_name,
+                    value_attr_two=well_label,
+                    properties=create_well_properties(run_name, well_label),
+                    properties_digest=create_well_properties_digest(
+                        run_name, well_label
+                    ),
                 )
-            ],
-        )
+            )
+        ],
     )
 
+    qcdb_session.add(seq_product)
     qcdb_session.commit()
+
+    return seq_product
 
 
 def update_qc_state(
     qc_status_post: QcStatus, qc_state_db: QcState, qcdb_session: Session
 ):
 
-    user = qcdb_session.execute(
-        select(User).where(User.username == qc_status_post.user)
-    ).scalar_one_or_none()
-    qc_state_db.user = user
-    qc_state_db.date_updated = datetime.now()
-    qc_state_db.id_qc_state_dict = qcdb_session.execute(
+    # Check that values are in the DB.
+    desired_qc_state_dict = qcdb_session.execute(
         select(QcStateDict.id_qc_state_dict).where(
             QcStateDict.state == qc_status_post.qc_state
         )
-    ).one_or_none()[0]
-    qc_state_db.id_qc_type = qcdb_session.execute(
+    ).one_or_none()
+    if desired_qc_state_dict is None:
+        raise Exception(
+            "Desired QC state is not in the QC database. It might not be allowed."
+        )
+
+    user = qcdb_session.execute(
+        select(User).where(User.username == qc_status_post.user)
+    ).scalar_one_or_none()
+    if user is None:
+        raise Exception(
+            "User has not been found in the QC database. Has it been registered?"
+        )
+
+    qc_type = qcdb_session.execute(
         select(QcType.id_qc_type).where(QcType.qc_type == qc_status_post.qc_type)
-    ).one_or_none()[0]
+    ).one_or_none()
+    if qc_type is None:
+        raise Exception("QC type is not in the QC database.")
+
+    qc_state_db.user = user
+    qc_state_db.date_updated = datetime.now()
+    qc_state_db.id_qc_state_dict = desired_qc_state_dict[0]
+    qc_state_db.id_qc_type = qc_type[0]
     qc_state_db.created_by = "LangQC"
     qc_state_db.is_preliminary = qc_status_post.is_preliminary
 
 
+def qc_status_json(db_qc_state: QcState) -> QcStatus:
+
+    return QcStatus(
+        user=db_qc_state.user.username,
+        date_created=db_qc_state.date_created,
+        date_updated=db_qc_state.date_updated,
+        qc_type=db_qc_state.qc_type.qc_type,
+        qc_type_description=db_qc_state.qc_type.description,
+        qc_state=db_qc_state.qc_state_dict.state,
+        is_preliminary=db_qc_state.is_preliminary,
+        created_by=db_qc_state.created_by,
+    )
+
+
 @router.post(
-    "/run/{run_name}/well/{well_label}/qc_assign", tags=["Well level QC operations"]
+    "/run/{run_name}/well/{well_label}/qc_assign",
+    tags=["Well level QC operations"],
+    response_model=QcStatus,
 )
 def assign_qc_status(
     run_name: str,
     well_label: str,
     qc_status: QcStatus,
     qcdb_session: Session = Depends(get_qc_db),
+    mlwhdb_session: Session = Depends(get_mlwh_db),
 ):
 
     qc_state = get_qc_state_for_well(run_name, well_label, qcdb_session)
@@ -178,6 +216,20 @@ def assign_qc_status(
         seq_product = get_seq_product_for_well(run_name, well_label, qcdb_session)
 
         if seq_product is None:
+            # Check that well exists in mlwh
+            mlwh_well = mlwhdb_session.execute(
+                select(PacBioRunWellMetrics).where(
+                    and_(
+                        PacBioRunWellMetrics.pac_bio_run_name == run_name,
+                        PacBioRunWellMetrics.well_label == well_label,
+                    )
+                )
+            ).scalar()
+            if mlwh_well is None:
+                raise HTTPException(
+                    status_code=400, detail="Well is not in the MLWH database."
+                )
+
             # Create a SeqProduct and related things for the well.
             seq_product = construct_seq_product_for_well(
                 run_name, well_label, qcdb_session
@@ -192,9 +244,19 @@ def assign_qc_status(
         qc_type = qcdb_session.execute(
             select(QcType).where(QcType.qc_type == "library")
         ).scalar_one_or_none()
+        if qc_type is None:
+            raise HTTPException(
+                status_code=400, detail="QC type is not in the QC database."
+            )
+
         qc_state_dict = qcdb_session.execute(
             select(QcStateDict).where(QcStateDict.state == qc_status.qc_state)
         ).scalar_one_or_none()
+        if qc_state_dict is None:
+            raise HTTPException(
+                status_code=400, detail="QC state dict is not in the QC database."
+            )
+
         qc_state = QcState(
             created_by="LangQC",
             is_preliminary=qc_status.is_preliminary,
@@ -224,6 +286,11 @@ def assign_qc_status(
         qcdb_session.commit()
 
         # Update the current record
-        update_qc_state(qc_status, qc_state, qcdb_session)
+        try:
+            update_qc_state(qc_status, qc_state, qcdb_session)
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
         qcdb_session.merge(qc_state)
         qcdb_session.commit()
+
+    return qc_status_json(qc_state)
