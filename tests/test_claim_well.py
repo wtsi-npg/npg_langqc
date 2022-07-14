0452a8c7d076d7f258bc6f8aa5e5e7e434297587
Adam Blanchet <ab59@sanger.ac.uk>
2022-07-14 10:47:44 +0000

    Add test for already claimed well.
    Signed-off-by: Adam Blanchet <ab59@sanger.ac.uk>

--- tests/test_claim_well.py
+++ tests/test_claim_well.py
@@ -79,3 +79,25 @@ def test_claim_well_unknown_user(test_client: TestClient, test_data_factory):
         response.json()["detail"]
         == "User has not been found in the QC database. Has it been registered?"
     )
+
+
+def test_error_on_already_claimed(test_client: TestClient, test_data_factory):
+    """Expect an error when claiming a well which has already been claimed"""
+
+    test_data = {
+        "MARATHON": {"A1": "Failed", "B1": "On hold"},
+        "SEMI-MARATHON": {"D1": "Claimed", "A1": "Passed"},
+    }
+    test_data_factory(test_data)
+
+    post_data = {"user": "zx80", "qc_type": "library"}
+
+    for run_name in test_data:
+        for well_label in test_data[run_name]:
+
+            response = test_client.post(
+                f"/pacbio/run/{run_name}/well/{well_label}/qc_claim",
+                json.dumps(post_data),
+            )
+            assert response.status_code == 400
+            assert response.json()["detail"] == "The well has already been claimed."
