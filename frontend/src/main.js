d6e6a708370e51b3dee089bc114523f7aee52d2a
Kieron Taylor <kt19@sanger.ac.uk>
2022-10-17 14:05:13 +0000

    Dud tab component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,9 @@ import router from './router'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
-import {Tabs, Tab} from 'vue3-tabs-component';
-
 import './assets/main.css'
 
-const app = createApp(App).component('tabs', Tabs).component('tab', Tab);
+const app = createApp(App);
 
 app.use(router);
 app.use(ElementPlus); // Configure global element options here
