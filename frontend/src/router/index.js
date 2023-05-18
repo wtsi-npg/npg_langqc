f8ff4a2b5a1db60fa645cf299fb1ed6f35288eb3
Kieron Taylor <kt19@sanger.ac.uk>
2023-05-18 10:44:24 +0000

    Introduce wells-by-run view, and connect to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WellsByStatus from '@/views/WellsByStatus.vue'
+import WellsByRun from '@/views/WellsByRun.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,12 @@ const router = createRouter({
       path: '/wells',
       name: 'WellsByStatus',
       component: WellsByStatus,
+    },
+    {
+      path: '/run/:runName',
+      name: 'WellsByRun',
+      component: WellsByRun,
+      props: true
     },
     {
       path: '/about',
---
+++ frontend/src/views/WellsByRun.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { onMounted, watch, ref, provide } from "vue"
+import { ElMessage } from "element-plus"
+import { useRoute, useRouter } from "vue-router"
+
+import LangQc from "@/utils/langqc"
+import WellTable from "@/components/WellTable.vue"
+import QcView from "@/components/QcView.vue"
+import QcControls from "@/components/QcControls.vue"
+
+import { generateUrl, qcQueryChanged } from "@/utils/url"
+import { useWellStore } from "@/stores/focusWell";
+import { getUserName } from "@/utils/session.js"
+
+const route = useRoute()
+const router = useRouter()
+const props = defineProps(["runName"])
+const focusWell = useWellStore()
+const serviceClient = new LangQc()
+
+let wellCollection = ref(null)
+let user = ref(null);
+let appConfig = ref(null)
+
+getUserName((email) => { user.value = email }).then();
+
+watch(() => route.query, (after, before) => {
+    if (qcQueryChanged(before, after) && (after['qcLabel'] || after['qcRun'])) {
+      focusWell.loadWellDetail(after.qcRun, after.qcLabel)
+    }
+  },
+  { immediate: true }
+)
+
+onMounted(() => {
+  serviceClient.getClientConfig().then(
+    data => {
+      appConfig.value = data
+    }
+  )
+
+  serviceClient.getWellsForRunPromise(props.runName).then(
+    (data) => wellCollection.value = data.wells
+  ).catch(error => {
+    ElMessage({
+      message: error.message,
+      type: "warning",
+      duration: 5000
+    })
+  })
+})
+
+provide('config', appConfig); // Needed by the QC widget
+
+function updateUrlQuery(newParams) {
+  // Merges the current URL query with new parameters
+  // The calling code does not need to replicate all properties from the
+  // previous URL query.
+  // route.query is read-only. We must copy it before modifying
+  const newUrl = generateUrl(Object.assign({}, route.query), newParams, route.path)
+
+  if (newUrl) {
+    router.push(newUrl)
+  }
+}
+
+function wellSelected(well) {
+  updateUrlQuery({qcRun: well.qcRun, qcLabel: well.qcLabel})
+}
+
+</script>
+
+<template>
+  <h2>Wells for run {{ props.runName }}</h2>
+  <WellTable :wellCollection="wellCollection" @wellSelected="wellSelected"/>
+  <h2>Well QC View</h2>
+  <div class="qcview" v-if="focusWell.runWell !== null">
+    <div class="data">
+      <QcView :runWell="focusWell.runWell" />
+    </div>
+    <aside class="controls">
+      <QcControls :user="user" />
+    </aside>
+  </div>
+  <div v-else>
+    <p>Well QC data will appear here</p>
+  </div>
+</template>
