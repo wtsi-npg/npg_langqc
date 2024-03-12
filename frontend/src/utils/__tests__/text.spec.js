fa7df49bf1951c0af46b76ade617009fd941cacd
mgcam <mg8@sanger.ac.uk>
2024-03-12 17:37:46 +0000

    Added study names tooltip on hover and highlighted the BIOSCAN study.
    mouseOver event for the well button displays a tooltip
    with names of studies.
    Study-aware well button colour scheme.

--- frontend/src/utils/__tests__/text.spec.js
+++ frontend/src/utils/__tests__/text.spec.js
@@ -1,7 +1,7 @@
 import { describe, test, expect } from 'vitest'
-import combineLabelWithPlate from '../text'
+import { combineLabelWithPlate, listStudiesForTooltip } from '../text'
 
-describe('Text processing', () => {
+describe('Well label and plate display', () => {
   test('Print well label without plate number', () => {
     expect(combineLabelWithPlate('A1', undefined)).toEqual('A1')
     expect(combineLabelWithPlate('A1', null)).toEqual('A1')
@@ -9,5 +9,20 @@ describe('Text processing', () => {
 
   test('Print well label that includes plate number', () => {
     expect(combineLabelWithPlate('A1', 1)).toEqual('1-A1')
+  })
+})
+
+describe('Study names tooltip', () => {
+  test('Display a warning in case of an empty study array', () => {
+    expect(listStudiesForTooltip([])).toEqual('No study info')
+  })
+
+  test('Display a single study name for an array of one study', () => {
+    expect(listStudiesForTooltip(['My single study'])).toEqual('My single study')
+  })
+
+  test('Display multiple lines for multiple studies', () => {
+    expect(listStudiesForTooltip(['Study One', 'Study two', 'Study 3'])).toEqual(
+      'Study One<br />Study two<br />Study 3')
   })
 })
